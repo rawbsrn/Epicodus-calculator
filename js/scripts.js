aa787bcbf04721c5f325078eb4baba4789951637
Rob Sameh <rob.sameh@gmail.com>
2022-01-24 20:39:49 -0800

    adding subtraction, multiplication, and division funcionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,50 @@
+// Business logic:
+
 function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// Everything below this line is user interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#add-output").text(number1 + " + " + number2 + " = " + result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#subtract-output").text(number1 + " - " + number2 + " = " + result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#multiply-output").text(number1 + " * " + number2 + " = " + result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#divide-output").text(number1 + " / " + number2 + " = " + result);
+  });
+});
